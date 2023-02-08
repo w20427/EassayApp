9dd5a8009b30a528df6d36b6dd914a36f512703d
Amit Kumar <w20427@gecwc.ac.in>
2023-02-08 16:05:09 +0530

    Adding files of project

---
+++ app/src/main/java/com/amit/EassyApp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.amit.EassyApp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnFirst=findViewById<Button>(R.id.button)
+        btnFirst.setOnClickListener {
+            // open in new Activity
+            intent=Intent(applicationContext,MainActivity2::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
